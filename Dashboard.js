69001d1e7cce48599447de2b1d37c34bec8cf88a
juls36363636 <jhgd148@gmail.com>
2025-05-12 21:20:13 -0600

    Add files via upload

---
+++ Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard de Pacientes</h1>
+      <p>Aquí irá la información de los pacientes.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
